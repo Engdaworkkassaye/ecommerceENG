130df4ff102572e0c734397465d8cb30c4f8a04c
Engdawork Kassaye <engdaworkkassaye6@gmail.com>
2024-02-06 14:05:52 -0600

    added model relation and seeds works

--- models/index.js
+++ models/index.js
@@ -1,17 +1,15 @@
-// import models
+
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, { through: ProductTag, foreignKey: 'tag_id' });
+Product.belongsToMany(Tag, { through: ProductTag, foreignKey: 'product_id' });
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 module.exports = {
   Product,
   Category,
